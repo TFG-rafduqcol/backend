20edccae64db4d57af336e7fdf9a4c2c5c2d907c
rafduqcol <rafduqcol@alum.us.es>
2025-06-15 18:19:34 +0200

    feat: add coverage of unit test

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
     testTimeout: 10000,
-  };
-  
+    collectCoverage: true,
+    coverageDirectory: '../coverage',
+    coverageReporters: ['json', 'lcov', 'text', 'clover'],
+    collectCoverageFrom: [
+        '../controllers/**/*.js',
+        '../models/**/*.js',
+        '../routes/**/*.js',
+        '../middlewares/**/*.js',
+        '!../**/index.js'
+    ]
+};
